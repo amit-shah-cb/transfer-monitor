0793e915dfc529df061a476de183b66406b0e98f
Amit Shah <amit.shah@coinbase.com>
2023-09-08 00:08:16 -0500

    unpack transfer and mint nft

--- app/api/hook/route.ts
+++ app/api/hook/route.ts
@@ -1,15 +1,41 @@
 import { NextResponse,NextRequest } from 'next/server'
 import { SmartContract, ThirdwebSDK} from "@thirdweb-dev/sdk";
 import { EthersWallet } from "@thirdweb-dev/wallets";
-import { ethers } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
 
 
+interface Log {
+  address: string;
+  topics: string[];
+  data: string;
+}
 
+interface TransferLog {
+  from: string;
+  to: string;
+  value: ethers.BigNumber;
+}
 
+function decodeERC20Transfers(logs: Log[]): TransferLog[] {
+  const iface = new ethers.utils.Interface([
+    'event Transfer(address indexed from, address indexed to, uint256 value)',
+  ]);
+
+  const transferLogs = logs.filter((log) => log.topics[0] === iface.getEventTopic('Transfer'));
+
+  return transferLogs.map((log) => {
+    const decodedLog = iface.decodeEventLog('Transfer', log.data, log.topics);
+    return {
+      from: decodedLog.from,
+      to: decodedLog.to,
+      value: decodedLog.value,
+    };
+  });
+}
 
 export async function POST(req:NextRequest) {  
   /*
-  {
+ {
   id: '7c6f5040-a376-4fa5-b5d0-51928866d007',
   event_type: 'ALERT',
   transaction: {
@@ -19,7 +45,7 @@ export async function POST(req:NextRequest) {
     hash: '0x4a2d9212cbc7d5187eefd4b8d6f6208b75d08bbde95b90a512c02f121d95f60a',
     from: '0x69bAdF095B03d62e97445EB0142e6488d19fF38B',
     to: '0xCB0B29999f1EC513992509625FF69655f667330A',
-    logs: [ [Object] ],
+    logs: [{"address":"0xCB0B29999f1EC513992509625FF69655f667330A","topics":["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef","0x00000000000000000000000069badf095b03d62e97445eb0142e6488d19ff38b","0x0000000000000000000000004c64c7dc4fc7ba5b89fad3aebc68892bfc1b67d5"],"data":"0x00000000000000000000000000000000000000000000003635c9adc5dea00000"}],
     input: '0xa9059cbb0000000000000000000000004c64c7dc4fc7ba5b89fad3aebc68892bfc1b67d500000000000000000000000000000000000000000000003635c9adc5dea00000',
     value: '0x0',
     nonce: '0x17',
@@ -32,30 +58,35 @@ export async function POST(req:NextRequest) {
   }
 }
 */
+
   const data = await req.json()
-  console.log(JSON.stringify(data.transaction.logs));
-  return NextResponse.json({});
+  const transferLog = decodeERC20Transfers(data.transaction.logs)
+  console.log("received transfers:",transferLog);
 
-  // let wallet = (ethers.Wallet.fromMnemonic(process.env.MNEMONIC as string));
-  // wallet=  wallet.connect(new ethers.providers.JsonRpcProvider(process.env.PROVIDER))
-  // console.log("loaded wallet:",await wallet.getAddress(), "balance:", await wallet.getBalance());
-
- 
-  // const sdk = ThirdwebSDK.fromSigner(wallet, process.env.CHAIN as string, {   
-  //   secretKey:process.env.THIRD_WEB_SECRET as string,
-  // });
-
-  // const contract  = await sdk.getContract(process.env.ERC1155_CONTRACT_ADDRESS as string);
-
-  // console.log("loaded contract:",await contract.getAddress());
-  // // Address of the wallet you want to send the NFT to
-  // const toAddress = wallet.address;
-  // const tokenId = 3; // The token ID of the NFT you want to send
-  // const amount = 1; // How many copies of the NFTs to transfer
+  let wallet = (ethers.Wallet.fromMnemonic(process.env.MNEMONIC as string));
+  wallet=  wallet.connect(new ethers.providers.JsonRpcProvider(process.env.PROVIDER))
+  console.log("loaded wallet:",await wallet.getAddress(), "balance:", await wallet.getBalance());
 
  
-  // const tx = await contract.erc1155.claimTo(toAddress, tokenId, amount);
-  // console.log(tx)
+  const sdk = ThirdwebSDK.fromSigner(wallet, process.env.CHAIN as string, {   
+    secretKey:process.env.THIRD_WEB_SECRET as string,
+  });
 
-  // return NextResponse.json(tx)
+  const contract  = await sdk.getContract(process.env.ERC1155_CONTRACT_ADDRESS as string);
+
+  console.log("loaded contract:",await contract.getAddress());
+  // Address of the wallet you want to send the NFT to
+  
+  const toAddress = transferLog[0].from
+  let tokenId=0;
+  if (transferLog[0].value.gt(BigNumber.from(50000))){
+    tokenId=1;
+  }// The token ID of the NFT you want to send
+  const amount = 1; // How many copies of the NFTs to transfer
+
+ 
+  const tx = await contract.erc1155.claimTo(toAddress, tokenId, amount);
+  console.log(tx)
+
+  return NextResponse.json(tx)
 }
